c8eb131e0e42f37da5a4196fd38390a2e7e1c962
Ross Thomson <rthomson412@gmail.com>
2023-06-11 15:38:25 -0400

    scss

---
+++ fun.js
@@ -0,0 +1,8 @@
+function printUserInfo({ name, age }) {
+    console.log("Name:", name);
+    console.log("Age:", age);
+  }
+  
+  const user = { name: "Alice", age: 25 };
+  printUserInfo(user);
+  
