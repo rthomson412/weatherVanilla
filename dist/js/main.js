c1d5e07f3df2c8fd46032fa97673590dd401aeab
Ross Thomson <rthomson412@gmail.com>
2023-06-15 16:47:21 -0400

    location button

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,22 @@
 import CurrentLocation from "./CurrentLocation.js";
+
+import {addSpinner} from "./domFunctions.js";
+
 const currentLoc = new CurrentLocation();
 
 const initApp = () => {
-
+// add listeners
+const geoButton = document.getElementById("getLocation");
+geoButton.addEventListener("click", getGeoWeather);
 }
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+const getGeoWeather = (event) => {
+    if (event && event.type === "click") {
+      const mapIcon = document.querySelector(".fa-location-dot");
+      addSpinner(mapIcon);
+    }
+    // if (!navigator.geolocation) return geoError();
+    // navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  };
